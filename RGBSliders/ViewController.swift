fcae9f8d140d0edec59cd244b2192a31278cba47
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-05 00:22:52 +0300

    optimization 1

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -27,21 +27,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         mainView.layer.cornerRadius = 5
-//        mainView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        setColor()
+        setValueForLabel()
+        setValueForTextField()
     }
     
     @IBAction func changeColor() {
+        setColor()
+        setValueForLabel()
+        setValueForTextField()
+    }
+    
+    func setColor() {
         mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                            green: CGFloat(greenSlider.value),
                                            blue: CGFloat(blueSlider.value),
                                            alpha: 1)
-        
+    }
+    
+    private func setValueForLabel() {
         redLabel.text = String(format: "%.2f", redSlider.value)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
-        
-        redTextField.text = String(format: "%.2f", redSlider.value)
-        greenTextField.text = String(format: "%.2f", greenSlider.value)
-        blueTextField.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    func setValueForTextField() {
+        redTextField.text = string(from: redSlider)
+        greenTextField.text = string(from: greenSlider)
+        blueTextField.text = string(from: blueSlider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        return String(format: "%.2f", slider.value)
     }
 }
